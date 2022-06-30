52ed65103ebb309457aa8846df99b4a307c553d3
Amod Mandal <mandala573@gmail.com>
2022-06-30 21:48:18 +0545

    second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Incremen',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
